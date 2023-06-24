e99138ace1d067b3d3f927563e44a12e39d20e9f
Anusha S <anushasridharan7@gmail.com>
2023-06-24 19:23:37 +0530

    updated navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="/" class="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-blue">FoodAware</span>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-blue">WasteAware</span>
       </a>
       <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium md:flex-row md:space-x-8 md:mt-0 md:border-0">
